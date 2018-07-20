6d0b12cfca052b7a99c65f70a16e51ab987567d6
davctv <davide.tasca@contrive.it>
2018-07-20 15:01:28 +0200

    Formatted test for esp32.com forum thread issue request

--- Makefile
+++ Makefile
@@ -3,7 +3,8 @@
 # project subdirectory.
 #
 
-PROJECT_NAME := hello-world
+PROJECT_NAME := test_spiffs_crash
+EXCLUDE_COMPONENTS := openssl aws_iot libsodium json jsmn console mdns esp-tls aes esp_http_client coap fatfs
 
 include $(IDF_PATH)/make/project.mk
 
--- main/main.c
+++ main/main.c
@@ -11,178 +11,171 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <ctype.h>
-#include <time.h>
 #include <dirent.h>
-#include <netdb.h>
-#include <sys/socket.h>
-
-
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
 #include "freertos/queue.h"
 
-#include "esp_system.h"
-#include "esp_wifi.h"
-#include "esp_event_loop.h"
 #include "esp_log.h"
-#include "esp_ota_ops.h"
-#include "rom/rtc.h"
-
-#include "nvs.h"
-#include "nvs_flash.h"
-
 #include "esp_spiffs.h"
-#include "esp_vfs_fat.h"
-//#include "driver/sdmmc_host.h"
-#include "sdmmc_cmd.h"
-
-#include "driver/uart.h"
-#include "driver/gpio.h"
-#include "driver/i2c.h"
-#include "driver/adc.h"
-#include "esp_adc_cal.h"
-#include "driver/ledc.h"
-#include "driver/rmt.h"
-
-#include "lwip/api.h"
-#include "lwip/err.h"
-#include "lwip/netdb.h"
-#include "lwip/dns.h"
 
 
 
 #define TAG "example"
 
+#define PARTITION_LABEL		"users"
+//Incresase this number and "boom"...program stack overflow..
+#define NUMBER_OF_UT_FILES 100
+
+DIR *mydir;
+struct dirent* entry;
+uint16_t counter = 0;
 
 void app_main()
 {
 
-	// Initialize NVS.
-	esp_err_t err = nvs_flash_init();
-	if (err == ESP_ERR_NVS_NO_FREE_PAGES) {
-		// OTA app partition table has a smaller NVS partition size than the non-OTA
-		// partition table. This size mismatch may cause NVS initialization to fail.
-		// If this happens, we erase NVS partition and initialize NVS again.
-		ESP_ERROR_CHECK(nvs_flash_erase());
-		err = nvs_flash_init();
-	}
-
+	esp_log_level_set(TAG, ESP_LOG_DEBUG);
+	esp_log_level_set("SPIFFS", ESP_LOG_DEBUG);
 
     ESP_LOGI(TAG, "Initializing SPIFFS system");
 
     esp_vfs_spiffs_conf_t conf = {
-      .base_path = "/D",
-      .partition_label = NULL,
+      .base_path = "/U",
+      .partition_label = PARTITION_LABEL,
       .max_files = 2,
       .format_if_mount_failed = true
     };
 
     // Use settings defined above to initialize and mount SPIFFS filesystem.
     // Note: esp_vfs_spiffs_register is an all-in-one convenience function.
-	esp_err_t ret1 = esp_vfs_spiffs_register(&conf);
+	esp_err_t ret = esp_vfs_spiffs_register(&conf);
 
-    if (ret1 != ESP_OK) {
-        if (ret1 == ESP_FAIL) {
+    if (ret != ESP_OK) {
+        if (ret == ESP_FAIL) {
             ESP_LOGE(TAG, "Failed to mount or format filesystem");
-        } else if (ret1 == ESP_ERR_NOT_FOUND) {
+        } else if (ret == ESP_ERR_NOT_FOUND) {
             ESP_LOGE(TAG, "Failed to find SPIFFS partition");
         } else {
-            ESP_LOGE(TAG, "Failed to initialize SPIFFS (%d)", ret1);
+            ESP_LOGE(TAG, "Failed to initialize SPIFFS (%d)", ret);
         }
     }
 
     size_t total = 0, used = 0;
-    ret1 = esp_spiffs_info(NULL, &total, &used);
-    if (ret1 != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%d)", ret1);
+    ret = esp_spiffs_info(PARTITION_LABEL, &total, &used);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%d)", ret);
     } else {
         ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
     }
 
 
-    for (uint16_t i = 0; i < 1000; i++)
+    ret = esp_spiffs_format(PARTITION_LABEL);
+
+    ESP_LOGI(TAG, "Partition formatted:%d", ret);
+
+    for (uint16_t i = 1; i <= NUMBER_OF_UT_FILES; i++)
     {
     	FILE *fp;
     	char string[100];
 
-    	sprintf(string, "/D/UT/file%03d", i);
+    	sprintf(string, "/U/UT/foo%03d", i);
 
 
     	fp = fopen(string, "w");
 
     	memset(string, 0x0, sizeof(string));
-    	sprintf(string,"This is the content of file%03d", i);
+    	sprintf(string,"This is the content of foo%03d", i);
 
     	fwrite(string, 1, strlen(string), fp);
 
     	fclose(fp);
 
-    	ESP_LOGD(TAG, "creating file:%03d", i);
+    	ESP_LOGD(TAG, "creating file /U/UT/file%03d", i);
     }
 
 
-	ESP_LOGD(TAG, "creating file in /D/UT: DONE");
+	ESP_LOGI(TAG, "creating file in /U/UT: DONE");
 
+	vTaskDelay(1000 / portTICK_RATE_MS);
+
+	fclose(fopen("/U/UE/bar", "w"));
+
+	ESP_LOGI(TAG, "creating file in /U/UE/bar: DONE");
 
 	vTaskDelay(1000 / portTICK_RATE_MS);
 
 
-	ESP_LOGD(TAG, "List files in \"folder\" /D");
 
-	DIR *mydir;
-	struct dirent* entry;
+	ESP_LOGI(TAG, "List files in \"folder\" /U");
+	ESP_LOGI(TAG, "freeHeap:%d; StackWaterMark:%d", heap_caps_get_free_size(MALLOC_CAP_8BIT), uxTaskGetStackHighWaterMark(NULL));
 
-	mydir = opendir("/D");
-
+	mydir = opendir("/U");
 
 	while ((entry = readdir(mydir)))
 	{
 
-		ESP_LOGD(TAG, "file list UT:%s", entry->d_name);
-
+		ESP_LOGD(TAG, "file list /U:%s", entry->d_name);
+		counter++;
 	}
 
 	closedir(mydir);
+
+	ESP_LOGI(TAG, "List files in \"folder\" /U DONE: num file %d", counter);
+
+
+
 
 
 	vTaskDelay(3000 / portTICK_RATE_MS);
 
 
-	ESP_LOGD(TAG, "List files in \"folder\" /D/UT");
+	ESP_LOGI(TAG, "List files in \"folder\" /U/UT");
+	ESP_LOGI(TAG, "freeHeap:%d; StackWaterMark:%d", heap_caps_get_free_size(MALLOC_CAP_8BIT), uxTaskGetStackHighWaterMark(NULL));
 
 
+	mydir = opendir("/U/UT");
 
-	mydir = opendir("/D/UT");
-
+	counter = 0;
 
 	while ((entry = readdir(mydir)))
 	{
-
-		ESP_LOGD(TAG, "file list UT:%s", entry->d_name);
-
+		ESP_LOGD(TAG, "file list /U/UT:%s", entry->d_name);
+		counter++;
 	}
 
 	closedir(mydir);
+
+	ESP_LOGI(TAG, "List files in \"folder\" /U/UT DONE: num file %d", counter);
 
 	vTaskDelay(3000 / portTICK_RATE_MS);
 
-	ESP_LOGD(TAG, "List files in \"folder\" /D/UE");
 
 
 
-	mydir = opendir("/D/UE");
 
+	ESP_LOGI(TAG, "List files in \"folder\" /U/UE");
+	ESP_LOGI(TAG, "freeHeap:%d; StackWaterMark:%d", heap_caps_get_free_size(MALLOC_CAP_8BIT), uxTaskGetStackHighWaterMark(NULL));
 
+	mydir = opendir("/U/UE");
+
+	counter = 0;
 	while ((entry = readdir(mydir)))
 	{
 
-		ESP_LOGD(TAG, "file list UE:%s", entry->d_name);
-
+		ESP_LOGD(TAG, "file list /U/UE:%s", entry->d_name);
+		counter++;
 	}
 
+	ESP_LOGI(TAG, "List files in \"folder\" /U/UE DONE: num file %d", counter);
+
 	closedir(mydir);
+
+	while(1)
+	{
+		vTaskDelay(1000 / portTICK_RATE_MS);
+	}
 
 
 }
